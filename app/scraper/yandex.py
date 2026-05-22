13c550020e790c7ccbafc9bdb2ee9ac2a2023a0f
Lebedeva2006 <ml7542190@gmail.com>
2026-05-22 21:28:32 +0300

    fix: chromedriver headless argument

--- app/scraper/yandex.py
+++ app/scraper/yandex.py
@@ -65,7 +65,8 @@ def search(query: str) -> List[Dict]:
     options.binary_location = "/usr/bin/google-chrome-stable"
     options.add_argument('--disable-blink-features=AutomationControlled')
     options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
-    driver = uc.Chrome(options=options, headless=True)
+    options.add_argument('--headless')
+    driver = uc.Chrome(options=options, version_main=148)
     driver.get(url)
 
     time.sleep(5)
